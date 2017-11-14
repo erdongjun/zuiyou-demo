0ddf8f7d7efd44b7f5878e22908ab9e1d920c11f
xifan <993656553@qq.com>
2017-11-14 16:58:41 +0800

    add upload function

--- application/home/controller/Home.php
+++ application/home/controller/Home.php
@@ -14,4 +14,18 @@ class Home extends Controller
 		# code...
 		# echo "init";
 	}
+	public function upload(){
+		    // 获取表单上传文件 
+		    $file = request()->file('file');
+		    // 移动到框架应用根目录/public/uploads/ 目录下
+	        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
+	        $name = $info->getSaveName();
+	        if($name){
+	            return json(['status'=>'1','url'=> '/public/uploads/'.$name,'msg'=>'上传成功']);
+	        }else{
+	            // 上传失败获取错误信息
+	        	return json(['status'=>'0','msg'=>$file->getError()]);
+	        }
+
+	}
 }
