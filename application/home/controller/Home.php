d738b23da7c69fb19ae7bbc2a6956dfb89a9782a
xifan <993656553@qq.com>
2017-11-15 11:53:29 +0800

    edit

--- application/home/controller/Home.php
+++ application/home/controller/Home.php
@@ -18,10 +18,12 @@ class Home extends Controller
 		    // 获取表单上传文件 
 		    $file = request()->file('file');
 		    // 移动到框架应用根目录/public/uploads/ 目录下
-	        $info = $file->move(ROOT_PATH . 'public' . DS . 'uploads');
-	        $name = $info->getSaveName();
-	        if($name){
-	            return json(['status'=>'1','url'=> '/public/uploads/'.$name,'msg'=>'上传成功']);
+	        $info = $file->validate(['ext'=>'jpg,png,gif,jpeg,mp3,mp4'])->move(ROOT_PATH . 'public' . DS . 'uploads');
+	        
+
+	        if($info){
+	        	$name = $info->getSaveName();
+	            return json(['status'=>'1','type'=>$file->getInfo()['type'],'url'=> '/public/uploads/'.$name,'msg'=>'上传成功']);
 	        }else{
 	            // 上传失败获取错误信息
 	        	return json(['status'=>'0','msg'=>$file->getError()]);
