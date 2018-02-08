db32112e8fd3482b0bfa6d73499d48425c616631
xifan <993656553@qq.com>
2018-02-08 11:55:06 +0800

    link

--- application/home/controller/Index.php
+++ application/home/controller/Index.php
@@ -22,32 +22,6 @@ class Index extends Home
         $this->assign('video_list',$video_list);
         return $this->fetch('index/index');
     }
-    public function cate()
-    {
-        $cate = new ArticleCate();
-        $cate_list = $cate ->cateTree();
-        $this->assign('cate_list',$cate_list);
-        return $this->fetch('index/cate');
-    }
-    public function article()
-    {
-        $id = $this->request->param('id');
-        $info = ArticleCate::get($id);
-        $list = Article::where(['status'=>1,'c_id'=>$id])->order('id desc')->paginate(20);
-        $this->assign('page',$list->render());
-        $this->assign('list',$list);
-        $this->assign('info',$info);
-        return $this->fetch('index/article');
-    }
-    public function detail()
-    {
-        $id = $this->request->param('id');
-        Article::where('id',$id)->setInc('read_num');
-        $info = Article::get($id);
-        $this->assign('info',$info);
-        
-        return $this->fetch('index/detail');
-    }
     // 网址导航
     public function link()
     {
@@ -55,11 +29,7 @@ class Index extends Home
                 'parent_id' => 0,
                 'type' => 1
             ]);
-
-
-
        $links = Link::all(['status'=>1]);
-
        foreach ($cates as $cate) {
             $list = [];
             foreach ($links as $link) {
@@ -70,10 +40,6 @@ class Index extends Home
 
             $cate['link_list'] = $list;
        }
-       // dump($cates);exit();
-
-
-        // $cate_list = $cate ->cateTree(1);
         $this->assign('cate_list',$cates);
         return $this->fetch('index/link');
     }
